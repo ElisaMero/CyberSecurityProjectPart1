39e1f2fdf6bab90aad7ac252cdf0254ea1c40c2c
Elisa Mero <elisa.mero@helsinki.fi>
2024-11-27 22:45:40 +0200

    insecureapp

---
+++ websovellus/registerpy.py
@@ -0,0 +1,225 @@
+from database import db
+from flask import session
+from sqlalchemy.sql import text
+from werkzeug.security import check_password_hash, generate_password_hash
+from random import randint
+
+
+def register(username, password):
+    try:
+        query = text(
+            "INSERT INTO users SET username='" + username + "', password='" + password + "'")
+        db.session.execute(query)
+        db.session.commit()
+    except:
+        return False
+    return login(username, password)
+
+
+# fix 2 and 4:
+# def register(username, password):
+#  hash_value = generate_password_hash(password)
+#  try:
+#     query = text("INSERT INTO users (username,password) VALUES (:username,:password)")
+#     db.session.execute(query, {"username": username, "password": hash_value})
+#     db.session.commit()
+#   except:
+#      return False
+#   return login(username, password)
+
+
+def login(username, password):
+    query = text("SELECT id, password FROM users WHERE username=:username")
+    result = db.session.execute(query, {"username": username})
+    user = result.fetchone()
+    if not user:
+        return False
+    else:
+        if check_password_hash(user.password, password):
+            session["user_id"] = user.id
+            return True
+        return False
+
+
+def user_id():
+    return session.get("user_id", 0)
+
+
+def logout():
+    del session["user_id"]
+
+
+def add_plant(name, latinname, light, water, other):
+    visibility = "visible"
+    check_name = text(
+        "SELECT name FROM plant WHERE user_id = :id AND visibility=:visibility")
+    result = db.session.execute(
+        check_name, {"id": session["user_id"], "visibility": visibility})
+    name_list = result.fetchall()
+    for i in name_list:
+        if i[0] == name:
+            return False
+    try:
+        query = text(
+            "INSERT INTO plant (name, latinname, light, water, other, user_id, visibility) VALUES (:name, :latinname, :light, :water, :other, :user_id, :visibility)")
+        db.session.execute(query, {"name": name, "latinname": latinname, "light": light,
+                           "water": water, "other": other, "user_id": session["user_id"], "visibility": visibility})
+        db.session.commit()
+    except:
+        return False
+    return True
+
+
+def count_plants():
+    visibility = "visible"
+    query = text(
+        "SELECT count(*) FROM plant WHERE user_id = :id AND visibility=:visibility")
+    result = db.session.execute(
+        query, {"id": session["user_id"], "visibility": visibility})
+    count = result.fetchone()
+    count = count[0]
+    if not count:
+        return 0
+    return count
+
+
+def show_user():
+    query = text("SELECT username FROM users WHERE id = :id")
+    result = db.session.execute(query, {"id": session["user_id"]})
+    name = result.fetchone()
+    name = name[0]
+    return name
+
+
+def plant_headings():
+    visibility = "visible"
+    query = text(
+        "SELECT name FROM plant WHERE user_id=:id AND visibility=:visibility")
+    result = db.session.execute(
+        query, {"id": session["user_id"], "visibility": visibility})
+    info = result.fetchall()
+    if info == []:
+        return " "
+    else:
+        info2 = []
+        for i in info:
+            info2.append(i[0])
+        return info2
+
+
+def plant_info(name):
+    visibility = "visible"
+    query = text(
+        "SELECT * FROM plant WHERE user_id =:id AND name =:name AND visibility=:visibility")
+    result = db.session.execute(
+        query, {"id": session["user_id"], "name": name, "visibility": visibility})
+    info = result.fetchall()
+    if info == []:
+        return " "
+    return info
+
+
+def add_notes(comment, name):
+    try:
+        query = text(
+            "INSERT INTO note1 (comment, user_id, name) VALUES (:comment, :user_id, :name)")
+        db.session.execute(
+            query, {"comment": comment, "user_id": session["user_id"], "name": name})
+        db.session.commit()
+    except:
+        return False
+    return True
+
+
+def get_comments(theplantname):
+    comment = text(
+        "SELECT comment FROM note1 WHERE user_id =:id AND name =:name")
+    result = db.session.execute(
+        comment, {"id": session["user_id"], "name": theplantname})
+    info = result.fetchall()
+    if info == []:
+        return " "
+    clean_info = []
+    for i in info:
+        clean_info.append(i[0])
+    return clean_info
+
+
+def create_emojis_table():
+    emoji_list = [127800, 127799, 127793, 127795, 127807]
+    for i in emoji_list:
+        try:
+            query = text("INSERT INTO emojis (number) VALUES (:number)")
+            db.session.execute(query, {"number": i})
+            db.session.commit()
+        except:
+            return False
+    return True
+
+
+def choose_emoji():
+    random = randint(1, 5)
+    query = text("SELECT number FROM emojis WHERE id =:id")
+    result = db.session.execute(query, {"id": random})
+    emoji = result.fetchone()
+    if emoji == []:
+        return 127807
+    for i in emoji:
+        emoji_code = i
+    return emoji_code
+
+
+def search_generator(input):
+    query = text(
+        "SELECT name FROM plant WHERE user_id =:id AND name LIKE :input and visibility = 'visible'")
+    result = db.session.execute(
+        query, {"id": session["user_id"], "input": "%"+input+"%"})
+    results = result.fetchall()
+    if results == []:
+        return " "
+    clean_info = []
+    for i in results:
+        clean_info.append(i[0])
+    return clean_info
+
+
+def delete_plant(name):
+    try:
+        query = text(
+            "UPDATE plant SET visibility= 'invisible' WHERE name=:name AND user_id = :user_id")
+        db.session.execute(
+            query, {"name": name, "user_id": session["user_id"]})
+        db.session.commit()
+    except:
+        return False
+    return True
+
+
+def create_direction_table():
+    name_list = [("Pothos", "The Pothos-genus is one of the easiest and most common housplants across the world. It is a very forgiving plant, meaning they are thriving with only a little watering and light. Different varietis of Pothos are visually appealing with their different and vibrant colors. They are also very fast-growing, when the conditions are met."),
+                 ("Snake plant", "Snake plants are highly beginner-friendly for neglecting plant parent. They tolrate drought well and only needs watering about once a month, depending on the amount light it gets.")]
+    for i in name_list:
+        try:
+            query = text(
+                "INSERT INTO directions (name, direction) VALUES (:name, :direction)")
+            db.session.execute(query, {"name": i[0], "direction": i[1]})
+            db.session.commit()
+        except:
+            return False
+    return True
+
+
+def show_pothos():
+    query = text(
+        "SELECT name, direction FROM directions WHERE name = 'Pothos'")
+    result = db.session.execute(query)
+    info = result.fetchall()
+    return info
+
+
+def show_snake():
+    query = text(
+        "SELECT name, direction FROM directions WHERE name = 'Snake plant'")
+    result = db.session.execute(query)
+    info = result.fetchall()
+    return info
---
+++ websovellus/routes.py
@@ -0,0 +1,176 @@
+from app import app
+import secrets
+from flask import redirect, render_template, request, session
+import registerpy
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/register")
+def new_registeration():
+    return render_template("register.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        # fix for flaw 5:
+        # username = request.form["username"]
+        # password = request.form["password"]
+        # confirmation = check_security(username, password)
+        # If confirmation:
+        # if registerpy.register(username, password):
+        # return redirect("/")
+        # return render_template("register.html", same_name=True)
+        # else:
+        # return render_template("register.html", same_name=False)
+        username = request.form["username"]
+        password = request.form["password"]
+        # fix 3: remove line 35!
+        question = request.form["question"]
+        if registerpy.register(username, password, question):
+            return redirect("/")
+        return render_template("register.html", same_name=True)
+
+# fix for flaw 5:
+# def check_security(username, password):
+#   if len(password) < 10 and username != password:
+#       continue
+#   else:
+#       return False
+#    upper = any(char.isupper() for char in password)
+#    lower = any(char.islower() for char in password)
+#    special = any(not char.isalnum() for char in password)
+#    number = any(char.isdigit() for char in password)
+#    return upper and lower and special and number
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("index.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if registerpy.login(username, password):
+            session["username"] = username
+            session["csrf_token"] = secrets.token_hex(16)
+            registerpy.create_emojis_table()
+            count = registerpy.count_plants()
+            name = registerpy.show_user()
+            plant_name = registerpy.plant_headings()
+            emoji = registerpy.choose_emoji()
+            return render_template("user.html", count=count, name=name, plant_name=plant_name, emoji=emoji)
+        return render_template("error.html")
+
+
+@app.route("/logout")
+def logout():
+    registerpy.logout()
+    return redirect("/")
+
+
+@app.route("/new_plant", methods=["GET", "POST"])
+def new_plant():
+    if request.method == "GET":
+        return render_template("plant.html")
+
+
+@app.route("/add_plant", methods=["GET", "POST"])
+def add_plant():
+    if request.method == "GET":
+        return render_template("plant.html")
+    if request.method == "POST":
+        if session["csrf_token"] != request.form.get("csrf_token"):
+            return render_template("error.html")
+        name = request.form["name"]
+        latinname = request.form["latinname"]
+        light = request.form["light"]
+        water = request.form["water"]
+        other = request.form["other"]
+        if registerpy.add_plant(name, latinname, light, water, other):
+            count = registerpy.count_plants()
+            name = registerpy.show_user()
+            plant_name = registerpy.plant_headings()
+            emoji = registerpy.choose_emoji()
+            return render_template("user.html", count=count, name=name, plant_name=plant_name, emoji=emoji)
+        return render_template("plant.html", same_name=True)
+
+
+@app.route("/headings", methods=["GET", "POST"])
+def heading_routes():
+    name = request.args.get("name")
+    theplantname = request.args.get("theplantname")
+    comment = registerpy.get_comments(theplantname)
+    info = registerpy.plant_info(theplantname)
+    count = registerpy.count_plants()
+    name = registerpy.show_user()
+    plant_name = registerpy.plant_headings()
+    emoji = registerpy.choose_emoji()
+    return render_template("user.html", info=info, count=count, name=name, plant_name=plant_name, comment=comment, emoji=emoji)
+
+
+@app.route("/notes", methods=["GET", "POST"])
+def add_notes():
+    if session["csrf_token"] != request.form.get("csrf_token"):
+        return render_template("error.html")
+    comment1 = request.form["plantname"]
+    theplantname = request.args.get("theplantname")
+    registerpy.add_notes(comment1, theplantname)
+    return redirect(f"/headings?theplantname={theplantname}")
+
+
+@app.route("/search", methods=["GET"])
+def result():
+    input = request.args.get("word")
+    plant_name = registerpy.search_generator(input)
+    count = registerpy.count_plants()
+    name = registerpy.show_user()
+    emoji = registerpy.choose_emoji()
+    return render_template("user.html", count=count, name=name, plant_name=plant_name, emoji=emoji)
+
+
+@app.route("/delete_plant")
+def delete_plant():
+    name = request.args.get("name")
+    print(name)
+    registerpy.delete_plant(name)
+    count = registerpy.count_plants()
+    name = registerpy.show_user()
+    emoji = registerpy.choose_emoji()
+    plant_name = registerpy.plant_headings()
+    return render_template("user.html", count=count, name=name, emoji=emoji,
+                           plant_name=plant_name)
+
+
+@app.route("/direction")
+def open_directions():
+    registerpy.create_direction_table()
+    return render_template("directions.html")
+
+
+@app.route("/pothos")
+def show_pothos():
+    info = registerpy.show_pothos()
+    return render_template("directions.html", info=info)
+
+
+@app.route("/snake")
+def show_snake():
+    info = registerpy.show_snake()
+    return render_template("directions.html", info=info)
+
+
+@app.route("/back")
+def go_back():
+    count = registerpy.count_plants()
+    name = registerpy.show_user()
+    emoji = registerpy.choose_emoji()
+    plant_name = registerpy.plant_headings()
+    return render_template("user.html", count=count, name=name, emoji=emoji,
+                           plant_name=plant_name)
